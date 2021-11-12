b64e262b32545a750253f37c691c522db01d25bb
Uthsav Chitra <uthsav@dynamic-oit-visitornet101-10-24-112-56.princeton.org>
2021-11-12 16:08:04 -0500

    fix some formatting

--- src/belayer.py
+++ src/belayer.py
@@ -12,32 +12,49 @@ from spatialcoord import *
 from tps import *
 
 
+##############################
+# PARSING ARGUMENTS
+##############################
+
 def construct_parser(parser):
     group = parser.add_mutually_exclusive_group(required=True)
+
     # input 10x directory
     group.add_argument('-i', '--indir', type=str, help="Input 10X directory for ST data.")
+
     # input count matrix and spatial coordinates as separate files
     group.add_argument('-s', '--stfiles', nargs=2, type=str, help="Input count matrix file followed by spatial coordinate file for ST data. Count matrix and spatial coordinate must have the same number of spots.")
+
     # running mode
     parser.add_argument('-m', '--mode', required=True, choices=["A", "R", "S", "L"], help="Running mode:\n\tA: axis-aligned layered tissue" + \
                         "\n\tR:rotated axis-aligned layered tissue" + \
                         "\n\tS:arbitrarily curved tissue supervised by annotated layers" + \
                         "\n\tL:layered tissue with linear layer boundaries")
-    # number layers
+
+    # number of layers
     parser.add_argument('-L', '--nlayers', required=True, type=int, help="Number of layers to infer.")
+
     # layer annotation (supervised mode)
     parser.add_argument('-a', '--annotation', type=str, help="Annotated layers for each spot when using S mode.")
+
     # output prefix
     parser.add_argument('-o', '--outprefix', type=str, default='belayer', help="Output prefix.")
+
     # platform
     parser.add_argument('-p', '--platform', choices=["ST","Visium"], default='Visium', help="Platform for spatial transcriptomics data. Only used when running mode is S.")
+
     return parser
     
+
+##############################
+# RUN
+##############################
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser = construct_parser(parser)
     args = parser.parse_args()
+
     # print parsed input info
     if args.mode == 'S' and (args.annotation is None):
         logger.error("Annotated layers file must exist when running mode is S.")
@@ -48,20 +65,33 @@ if __name__ == "__main__":
     logger.info("annotation={}".format(args.annotation))
     logger.info("outprefix={}".format(args.outprefix))
     logger.info("platform={}".format(args.platform))
-    ##### step 1: parse ST data input #####
+
+    ############################################################
+    # STEP 1: parse ST data input
+    ############################################################
+
     if not (args.indir is None):
         count, pos, barcodes, genes = read_input_10xdirectory(args.indir)
     else:
         count, pos, barcodes, genes = read_input_st_files(args.stfiles)
-    ##### step 2: identify layers by belayer according to input mode #####
+
+    ############################################################
+    # STEP 2: identify layers by belayer according to input mode
+    ############################################################
+
+    # AXIS-ALIGNED
     if args.mode == 'A':
         xcoords = pos[:,0]
         pooled_int_data, pooled_xcoords, map_1d_bins_to_2d = pool_data(count, xcoords)
         layer_pooled, layer_2d = dp(pooled_count, pooled_xcoords, args.nlayers, map_1d_bins_to_2d)
+
+    # ROTATED
     elif args.mode == 'R':
         angle, xcoords = find_rotation_angle(count, pos, args.nlayers)
         pooled_int_data, pooled_xcoords,  map_1d_bins_to_2d = pool_data(count, xcoords)
         layer_pooled, layer_2d = dp(pooled_count, pooled_xcoords, args.nlayers, map_1d_bins_to_2d)
+
+    # SUPERVISED
     elif args.mode == 'S':
         spos = spatialcoord(x=pos[:,0], y=pos[:,1], platform=args.platform)
         annot = pd.read_csv(args.annotation) # this file reading function need to be checked
@@ -69,13 +99,28 @@ if __name__ == "__main__":
         xcoords = tps.interpolation
         pooled_int_data, pooled_xcoords, map_1d_bins_to_2d = pool_data(count, xcoords)
         layer_pooled, layer_2d = dp(pooled_count, pooled_xcoords, args.nlayers, map_1d_bins_to_2d)
-    else:
+
+    # LINEAR BOUNDARIES
+    elif args.mode == 'L':
         logger.error("L mode to be implemented.")
-    ##### step 3: estimating piecewise linear function coefficients for individual genes #####
+
+    # invalid INPUT MODE
+    else:
+        logger.error("Invalid input mode.")
+
+    #################################################################################
+    # STEP 3: estimating piecewise linear function coefficients for individual genes
+    #################################################################################
+
     # TBD
-    ##### step 4: output the inferred layer and piecewise linear function info #####
-    # identified layer
+
+    #################################################################################   
+    # STEP 4: output the inferred layers and expression functions
+    #################################################################################
+
+    # OUTPUT inferred layers
     df_layer = pd.DataFrame( {"xcoords":xcoords, "layer":layer_2d}, index=barcodes )
     df_layer.to_csv(args.outprefix + "_layer.csv")
-    # piecewise linear function coefficient
+
+    # OUTPUT expression functions (I.e. piecewise linear function coefficient)
     # TBD
--- src/dprelated.py
+++ src/dprelated.py
@@ -2,23 +2,41 @@ import numpy as np
 from glmpca import glmpca
 
 
-def opt_linear(y, xcoords):
-    '''
-    y has size g x t
-    each row is being fit simultaneously
-    '''
-    g,n = y.shape
-    if n==1:
+# INPUT:
+# Y: matrix of size G x T
+# xcoords: list of T x-coordinates
+
+# OUTPUT:
+# Fit each row y_g of Y as y_g = xcoords * theta + const using linear regression
+# (1) theta, const for each gene g
+# (2) Total loss \sum_g ||y_g - yhat_g||^2 
+def opt_linear(Y, xcoords):
+    G,T = Y.shape
+
+    # if T=1, can always fit
+    if T==1:
         return np.array([]),0
-    X=np.ones((n,2))
+
+    # if T > 1, compute using linear regression
+    # theta has size 2 x G
+    # each col is the linreg coefficients for gene g
+
+    X=np.ones((T,2))
     X[:,0]=xcoords
-    theta=np.linalg.inv(X.T @ X) @ X.T @ y.T
-    error=np.linalg.norm(X @ theta - y.T)**2
-    # linreg coefficients theta.T have size g x 2
-    # each row is the linreg coefficients for gene g
+    theta=np.linalg.inv(X.T @ X) @ X.T @ Y.T
+    error=np.linalg.norm(X @ theta - Y.T)**2
+    
     return theta.T,error
 
 
+# INPUT:
+# Y: matrix of size G x T
+# xcoords: list of T x-coordinates
+
+# OUTPUT:
+# Fit each row y_g of Y as y_g = const
+# (1) const for each gene g
+# (2) Total loss \sum_g ||y_g - yhat_g||^2 
 def opt_const(y, xcoords):
     '''
     y has size g x t
@@ -36,22 +54,29 @@ def dp_raw(data, k, xcoords, opt_function=opt_linear, min_seg_size=3):
     '''
     G=data.shape[0]
     T=data.shape[1]
+
     # dp on matrix A of size num_times x k
     # where A[t,p] = error from using linreg to fit first t+1 times using (p+1)-part segmented regression
     error_mat=np.zeros((T,k))
+
     # map (t,p) -> (t', p-1) where you use p-1 segments for times 1,...,t' and a segment for t'+1,...,t
     segment_map={}
+
     # saved opt_function
     saved_opt_functions=np.zeros((T+1,T+1)) - 1
+
     # fill out first column of matrix (0th row is just 0)
     for t in range(min_seg_size-1,T):
         xc=xcoords[:t+1]
         _,err=opt_function(data[:,:t+1],xc)
         error_mat[t,0]=err
+
     # fill out each subsequent column p
     for p in range(1,k):
+
         # for each column, go from top to bottom [ignoring first row]
         colp_starting=(p+1)*min_seg_size-1
+
         for t in range(colp_starting,T):
             best_tprime=-1
             best_error=np.Inf
@@ -68,6 +93,7 @@ def dp_raw(data, k, xcoords, opt_function=opt_linear, min_seg_size=3):
                     best_tprime=tprime
             error_mat[t,p] = best_error
             segment_map[(t,p)] = (best_tprime,p-1)
+
     return error_mat, segment_map
 
 
@@ -75,13 +101,16 @@ def dp(pooled_count, pooled_xcoord, L, map_1d_bins_to_2d):
     G = pooled_count.shape[0]
     N_1d = pooled_count.shape[1]
     N = np.sum([len(v) for v in map_1d_bins_to_2d.values()])
+
     glmpca_res_1d_poisson = glmpca.glmpca(pooled_count, 2*L, fam="poi", penalty=100, verbose=False)
     F_glmpca_1d_poisson = glmpca_res_1d_poisson['factors']
     error_mat, seg_map = dp_raw(F_glmpca_1d_poisson, L, pooled_xcoords)
+    
     # get DP labels
     segs = find_segments_from_dp(error_mat, seg_map, L)
     layer_pooled = np.zeros(N_1d, dtype=np.int)
     layer_2d = np.zeros(N, dtype=np.int)
+    
     c=0
     for seg in segs:
         layer_pooled[ np.array(seg) ] = c
